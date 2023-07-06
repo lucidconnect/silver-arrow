8ff471543f8232e89c1076e02c7750ff2950cedb
olawolu <gbemigaolawolu@gmail.com>
2023-07-06 10:48:17 +0100

    feat: implement bundler & paymaster rpc calls

---
+++ bundlerclient/bundlerclient.go
@@ -0,0 +1,109 @@
+package bundlerclient
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/pkg/errors"
+)
+
+type Client struct {
+	ctx  context.Context
+	c, p *ethclient.Client
+}
+
+type GasEstimateResult struct {
+	PreVerificationGas *big.Int `json:"PreVerificationGas"`
+	VerificationGas    *big.Int `json:"VerificationGas"`
+	CallGasLimit       *big.Int `json:"CallGasLimit"`
+}
+
+func Dial(url, paymasterUrl string) (*Client, error) {
+	var client, paymasterClient *ethclient.Client
+	ctx := context.Background()
+
+	client, err := ethclient.Dial(url)
+	if err != nil {
+		log.Fatal("Oops! Something went wrong ", err)
+		return nil, err
+	}
+
+	if paymasterUrl != "" {
+		fmt.Println("initialising with a paymaster")
+		paymasterClient, err = ethclient.Dial(paymasterUrl)
+		if err != nil {
+			log.Fatal("Oops! Something went wrong ", err)
+			return nil, err
+		}
+	}
+
+	return newClient(ctx, client, paymasterClient), nil
+}
+
+func newClient(ctx context.Context, client, paymasterClient *ethclient.Client) *Client {
+	return &Client{
+		ctx: ctx,
+		c:   client,
+		p:   paymasterClient,
+	}
+}
+
+// GetAccountCode is a wrapper around ethclient.CodeAt()
+func (nc *Client) GetAccountCode(address common.Address) ([]byte, error) {
+	return nc.c.CodeAt(nc.ctx, address, nil)
+}
+
+func (nc *Client) GetAccountNonce(address common.Address) (uint64, error) {
+	return nc.c.NonceAt(nc.ctx, address, nil)
+}
+
+// SendUserOperation sends a user operation to an alt mempool
+func (nc *Client) SendUserOperation(entryPoint string, userop interface{}) (string, error) {
+	var result string
+
+	err := nc.c.Client().CallContext(nc.ctx, &result, "eth_sendUserOperation", userop, entryPoint)
+	if err != nil {
+		err = errors.Wrap(err, "eth_sendUserOperation call error")
+		return result, err
+	}
+
+	return result, nil
+}
+
+// Estimate the gas parameters required for the user operation
+func (nc *Client) EstimateUserOperationGas(entrypointAddress string, userop interface{}) (*GasEstimateResult, error) {
+	result := &GasEstimateResult{}
+
+	err := nc.c.Client().CallContext(nc.ctx, result, "eth_estimateUserOperationGas", userop, entrypointAddress)
+	if err != nil {
+		err = errors.Wrap(err, "eth_estimateUserOperationGas call error")
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// Estimate the gas parameters required for the user operation
+func (nc *Client) GetBalance(address string) (*big.Int, error) {
+	return nc.c.BalanceAt(nc.ctx, common.HexToAddress(address), nil)
+}
+
+// GetUserOperationByHash calls eth_getUserOperationByHash bundler rpc method
+func (nc *Client) GetUserOperationByHash(userophash string) error {
+	var result map[string]interface{}
+
+	err := nc.c.Client().CallContext(nc.ctx, &result, "eth_getUserOperationByHash", userophash)
+	if err != nil {
+		err = errors.Wrap(err, "eth_getUserOperationByHash call error")
+		return err
+	}
+
+	fmt.Println("user operation - ", result)
+	return nil
+}
+
+// eth_getUserOperationReciept
---
+++ bundlerclient/paymaster.go
@@ -0,0 +1,26 @@
+package bundlerclient
+
+import (
+	"fmt"
+	"log"
+)
+
+type PaymasterResult struct {
+	PaymasterAndData     string `json:"paymasterAndData"`
+	PreVerificationGas   string `json:"preVerificationGas"`
+	VerificationGasLimit string `json:"verificationGasLimit"`
+	CallGasLimit         string `json:"callGasLimit"`
+}
+
+func (nc *Client) SponsorUserOperation(entryPoint string, userop, pc interface{}) (*PaymasterResult, error) {
+	result := &PaymasterResult{}
+
+	err := nc.p.Client().CallContext(nc.ctx, result, "pm_sponsorUserOperation", userop, entryPoint, pc)
+	if err != nil {
+		log.Printf("pm_sponsorUserOperation -  message: %v \n", err)
+		return nil, err
+	}
+
+	fmt.Println("pm_sponsorUserOperation - ", result)
+	return result, nil
+}
