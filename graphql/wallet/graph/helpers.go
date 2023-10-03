64f9ae0318beab2d1c68328f501b50f26da58199
olawolu <gbemigaolawolu@gmail.com>
2023-10-03 09:55:14 +0100

    make auth configurable

--- graphql/wallet/graph/helpers.go
+++ graphql/wallet/graph/helpers.go
@@ -2,6 +2,8 @@ package graph
 
 import (
 	"context"
+	"os"
+	"strconv"
 
 	"github.com/helicarrierstudio/silver-arrow/repository/models"
 	"github.com/helicarrierstudio/silver-arrow/service/merchant"
@@ -9,6 +11,12 @@ import (
 )
 
 func getAuthenticatedAndActiveMerchant(ctx context.Context) (*models.Merchant, error) {
+	useAuthStr := os.Getenv("USE_AUTH")
+	useAuth, _ := strconv.ParseBool(useAuthStr)
+	if !useAuth {
+		return &models.Merchant{}, nil
+	}
+
 	merchant, err := merchant.ForContext(ctx)
 	if err != nil {
 		err = errors.Wrapf(err, "merchant authorization failed %v", ctx)
